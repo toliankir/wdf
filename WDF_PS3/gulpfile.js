c3b3b90c4465286e7badf015e51f5a5b19d3e6e6
toliankir@gmail.com <toliankir@gmail.com>
2019-01-23 00:33:32 +0200

    New gulp configuration

--- WDF_PS3/gulpfile.js
+++ WDF_PS3/gulpfile.js
@@ -1,225 +1,143 @@
 const gulp = require('gulp'),
-    sequence = require('run-sequence'),
-    watch = require('gulp-watch'),
-    rename = require('gulp-rename'),
-    less = require('gulp-less'),
-    prefixer = require('gulp-autoprefixer'),
     browserSync = require('browser-sync'),
-    debug = require('gulp-debug'),
-    cahced = require('gulp-cached'),
-    inject = require('gulp-inject'),
-    npm = require('main-npm-files'),
-    jshint = require('gulp-jshint'),
-    plumber = require('gulp-plumber'),
-    htmlhint = require('gulp-htmlhint'),
-    minifycss = require('gulp-minify-css'),
-    concat = require('gulp-concat'),
-    clean = require('gulp-clean'),
-    strip = require('gulp-strip-comments'),
-    uglify = require('gulp-uglify-es').default;
+    sequence = require('run-sequence'),
+    plugins = require('gulp-load-plugins')();
 
-const path = {
-    srcHTML: './src',
-    srcLess: './src/less',
-    srcJS: './src/js',
-
-    tmpHTML: './tmp',
-    tmpCSS: './tmp/style',
-    tmpJS: './tmp/js',
-
-    libs: '/libs',
-
-    dist: './dist',
-    distFont: './dist/webfonts',
-    distCSS: './dist/style',
-    distJS: './dist/js'
+const mask = {
+    html: '*.html',
+    less: '**/*.less',
+    css: '**/*.css',
+    js: '**/*.js'
 };
 
+const path = {
+    main: '.',
+    src: './src',
+    srcJs: './src/js',
+    srcLess: './src/less',
+    dist: './dist',
+    distVendor: './dist/vendor',
+    assets: './assets'
+};
 
-//------------------- Dev -------------------
-gulp.task('remove-all', () => {
-    return gulp.src(path.tmpHTML + '/*')
-        .pipe(clean());
+//Removes prev distributive before start.
+gulp.task('remove-dist', () => {
+    return gulp.src(path.dist, {read: false})
+        .pipe(plugins.clean());
 });
 
-//---------------- Own files ----------------
-gulp.task('less', () => {
-    return gulp.src(path.srcLess + '/**/*.less')
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(prefixer())
-        .pipe(gulp.dest(path.tmpCSS))
-        // .pipe(browserSync.reload({stream: true}))
-        .pipe(cahced('less'))
-        .pipe(debug({
-            title: 'LESS:',
+//-------------------------- Vendor --------------------------
+gulp.task('vendor-npm', () => {
+    gulp.src(plugins.mainNpmFiles())
+        .pipe(plugins.uglifyEs.default())
+        .pipe(plugins.rename({suffix: '.min'}))
+        .pipe(plugins.debug({
+            title: 'NPM JS:',
             showCount: false
-        }));
-});
-
-gulp.task('inject-css', ['less'], () => {
-    return gulp.src(path.tmpHTML + '/**/*.html')
-        .pipe(inject(gulp.src([
-            path.tmpCSS + '/**/*.css',
-            '!' + path.tmpCSS + path.libs + '/**/*'
-        ]), {
-            relative: true,
-            quiet: true
         }))
-        .pipe(gulp.dest(path.tmpHTML));
-});
+        .pipe(gulp.dest(path.distVendor));
 
-gulp.task('js', () => {
-    return gulp.src(path.srcJS + '/**/*.js')
-        .pipe(plumber())
-        .pipe(jshint(false))
-        .pipe(jshint.reporter('default'))
-        .pipe(gulp.dest(path.tmpJS))
-        .pipe(cahced('js'))
-        .pipe(debug({
-            title: 'JS: ',
+    gulp.src(plugins.styleNpmFiles())
+        .pipe(plugins.minifyCss())
+        .pipe(plugins.rename({suffix: '.min'}))
+        .pipe(plugins.debug({
+            title: 'NPM CSS:',
             showCount: false
-        }));
-});
-
-gulp.task('inject-js', ['js'], () => {
-    return gulp.src(path.tmpHTML + '/**/*.html')
-        .pipe(inject(gulp.src([
-            path.tmpJS + '/**/*.js',
-            '!' + path.tmpJS + path.libs + '/**/*'
-        ]), {
-            relative: true,
-            quiet: true
         }))
-        .pipe(gulp.dest(path.tmpHTML));
+        .pipe(gulp.dest(path.distVendor));
 });
 
-gulp.task('html', ['remove-all'], () => {
-    return gulp.src(path.srcHTML + '/**/*.html')
-        .pipe(plumber())
-        .pipe(htmlhint())
-        .pipe(htmlhint.failReporter())
-        .pipe(gulp.dest(path.tmpHTML))
-        .pipe(cahced('HTML'))
-        .pipe(debug({
-            title: 'HTML: ',
+//------------------------- /Vendor --------------------------
+
+//------------------------ Own Files -------------------------
+gulp.task('own-js', () => {
+    return gulp.src(path.srcJs + '/' + mask.js)
+        .pipe(plugins.plumber())
+        .pipe(plugins.jshint(false))
+        .pipe(plugins.jshint.reporter('default'))
+        .pipe(plugins.concat('script.min.js'))
+        .pipe(plugins.uglifyEs.default())
+        .pipe(plugins.debug({
+            title: 'Own JS:',
             showCount: false
-        }));
-});
-//---------------- /Own files ----------------
-
-//-------------------- NPM -------------------
-gulp.task('npm-css', () => {
-    return gulp.src(npm('dist/**/*.css'))
-        .pipe(gulp.dest(path.tmpHTML + path.libs))
-        .pipe(cahced('npm-css'))
-        .pipe(debug({
-            title: 'NPM-CSS: ',
-            showCount: false
-        }));
-});
-
-gulp.task('npm-js', () => {
-    return gulp.src(npm('dist/**/*.js'))
-        .pipe(gulp.dest(path.tmpHTML + path.libs))
-        .pipe(cahced('npm-js'))
-        .pipe(debug({
-            title: 'NPM JS: ',
-            showCount: false
-        }));
-});
-
-gulp.task('inject-npm', ['npm-js', 'npm-css'], () => {
-    return gulp.src(path.tmpHTML + '/**/*.html')
-        .pipe(inject(gulp.src([
-            path.tmpHTML + path.libs + '/**/*.css',
-            path.tmpHTML + path.libs + '/**/*.js',
-        ]), {
-            relative: true,
-            name: 'libs',
-            quiet: true
         }))
-        .pipe(gulp.dest(path.tmpHTML));
+        .pipe(gulp.dest(path.dist));
 });
-//------------------- /NPM -------------------
+
+gulp.task('own-less', () => {
+    return gulp.src(path.srcLess + '/' + mask.less)
+        .pipe(plugins.less())
+        .pipe(plugins.autoprefixer())
+        .pipe(plugins.minifyCss())
+        .pipe(plugins.concat('style.min.css'))
+        .pipe(plugins.debug({
+            title: 'Own LESS:',
+            showCount: false
+        }))
+        .pipe(gulp.dest(path.dist));
+});
+
+gulp.task('own-html', () => {
+    return gulp.src(path.src + '/' + mask.html)
+        .pipe(plugins.plumber())
+        .pipe(plugins.htmlhint())
+        .pipe(plugins.htmlhint.failReporter());
+});
+//----------------------- /Own Files -------------------------
+
+//Injects all files from distributive folder to html files.
 gulp.task('inject-all', () => {
-    sequence('html', 'inject-css', 'inject-js', 'inject-npm',  () => {
-        browserSync.reload();
-    });
+    return gulp.src([path.src + '/' + mask.html])
+        .pipe(plugins.inject(gulp.src(path.dist + '/*.css', {read: false}), {quiet: true}))
+        .pipe(plugins.debug({
+            title: 'HTML inject:',
+            showCount: false
+        }))
+        .pipe(plugins.inject(gulp.src(path.dist + '/*.js', {read: false}), {quiet: true}))
+        .pipe(plugins.inject(gulp.src(path.distVendor + '/' + mask.js, {read: false}), {name: 'libs', quiet: true}))
+        .pipe(plugins.inject(gulp.src(path.distVendor + '/' + mask.css, {read: false}), {name: 'libs', quiet: true}))
+        .pipe(gulp.dest(path.main));
 });
 
 gulp.task('browser-sync', () => {
     browserSync({
         server: {
-            baseDir: path.tmpHTML
+            baseDir: path.main
         },
         logFileChanges: false,
         notify: false
     });
 });
 
-gulp.task('default', ['browser-sync', 'inject-all'], () => {
-    return watch([
-        path.srcLess + '**/*.less',
-        path.srcJS + '/**/*.js',
-        path.srcHTML + '/**/*.html',
-        './package.json'
-    ], () => {
-        sequence('inject-all');
-    })
-});
-//------------------ /Dev -------------------
-
-//------------------ Build ------------------
-gulp.task('remove-dist', () => {
-    return gulp.src(path.dist + '/*')
-        .pipe(clean());
-});
-//---------------- Build CSS ----------------
-gulp.task('build-css', () => {
-    return gulp.src([
-        path.tmpHTML + path.libs + '/**/*.css',
-        path.tmpCSS + '/**/*.css'
-    ])
-        .pipe(minifycss())
-        .pipe(concat('style.css'))
-        .pipe(gulp.dest(path.distCSS));
+//Run all tasks in selected order
+gulp.task('build', () => {
+    return sequence('remove-dist', ['vendor-npm', 'own-js', 'own-less', 'own-html'], 'inject-all', 'browser-sync');
 });
 
-gulp.task('build-font', () => {
-    return gulp.src([
-        path.tmpHTML + path.libs + '/**/*.ttf',
-        path.tmpHTML + path.libs + '/**/*.woff2'
-    ])
-        .pipe(rename({dirname: ''}))
-        .pipe(debug())
-        .pipe(gulp.dest(path.distFont));
-});
-//--------------- /Build CSS ----------------
 
-//---------------- Build JS -----------------
+gulp.task('watch', ['build'], () => {
+    plugins.watch(path.srcLess + mask.less, () => {
+        sequence('own-less', 'inject-all', () => {
+            browserSync.reload();
+        })
+    });
 
-gulp.task('build-js', () => {
-    return gulp.src([
-        path.tmpHTML + path.libs + '/**/*.js',
-        path.tmpJS + '/**/*.js'
-    ])
-        .pipe(uglify())
-        .pipe(concat('bundle.js'))
-        .pipe(gulp.dest(path.distJS));
+    plugins.watch(path.srcJs + mask.js, () => {
+        sequence('own-js', 'inject-all', () => {
+            browserSync.reload();
+        })
+    });
+
+    plugins.watch(path.main + mask.html, () => {
+        sequence('own-html', 'inject-all', () => {
+            browserSync.reload();
+        })
+    });
 });
 
-//--------------- /Build JS -----------------
-
-gulp.task('pre-build', () => {
-    sequence('remove-dist', ['build-css', 'build-font', 'build-js']);
+//-------------------------- Help ----------------------------
+gulp.task('default', () => {
+    console.log('Run \'gulp --silent watch\' for working. \n' +
+        'That collects all dependencies and user files to distributive folder.\n' +
+        'Then starts web server on 3000 port and watching for user files changing.');
 });
-
-gulp.task('build', ['pre-build'], () => {
-    return gulp.src(path.srcHTML + '/**/*.html')
-        .pipe(inject(gulp.src(path.distJS + '/**/*.js', {read: false}), {relative: false, ignorePath: 'dist'}))
-        .pipe(inject(gulp.src(path.distCSS + '/**/*.css', {read: false}), {relative: false, ignorePath: 'dist'}))
-        .pipe(strip())
-        .pipe(gulp.dest(path.dist));
-});
-//----------------- /Build ------------------
